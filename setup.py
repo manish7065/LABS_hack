c9cad701b01283a2cf8e6c9d06e435354ceb4ac9
manish <manumi296@gmail.com>
2023-01-28 23:39:15 +0530

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="photo_editor_detection",
+    version="0.1.0",
+    author="Manish Kumar",
+    author_email="maimage95@gmail.com",
+    packages=find_packages()
+    
+)
